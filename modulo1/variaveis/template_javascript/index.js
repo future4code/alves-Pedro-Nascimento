2f1e99ccb213af2388a57a5f37a396239b05c8df
pedro Vinicius <pedro.carvalho161@gmail.com>
2022-05-09 20:35:58 -0300

    meu codigokk

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,41 @@
+//atividade1
+
+let seuNome = undefined
+let suaIdade = undefined
+
+typeof seuNome // string
+
+typeof suaIdade //numb.nome
+
+console.log(typeof seuNome, suaIdade)
+
+seuNome= prompt("Qual o seu login?")
+
+suaIdade= prompt("Quantos anos você tem?")
+
+console.log("Olá", seuNome, "minha idade", suaIdade)
+
+// Atividade 2.
+
+let a = 10
+let b = 25
+
+// Aqui faremos uma lógica para trocar os valores.
+
+let c= b// Variavel C pra recebr valor de A ou B(na hora eu descido).
+
+
+b = a
+a = c
+c = b
+
+
+
+
+
+
+
+
+
+
+
