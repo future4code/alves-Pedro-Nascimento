8fdd504fb9a899beea5fe99b70f1cf84984dc767
pedro Vinicius <pedro.carvalho161@gmail.com>
2022-05-10 19:45:28 -0300

    exercicios , nao cosnegui executar o exercicio 3/4, vou revisar e pedir ajuda

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,56 @@
+
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2(true)
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado)
+//**True E False = False
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) 
+// **True E false E True = False
+
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+//True
+//(!=)= Diferente.
+//!resultado= true E (boll1 Ou Bool2)= true (True ou false =True) e True && True = TRUE
+
+
+// console.log("d. ", typeof resultado
+
+//let primeiroNumero = prompt("Digite um numero!")
+// segundoNumero = prompt("Digite outro numero!")
+
+//const soma = primeiroNumero + segundoNumero
+//Number(primeiroNumero)
+//Number(segundoNunero)
+
+//transormar uma as string em Number
+//
+
+//console.log(soma)
+// transformando a string em numero da tudo ok.
+
+let idade = undefined
+typeof idade (Number)
+prompt("qual é a sua idade?", idade) 
+prompt("qual é a idade do seu melhor amigo?", idade)
+prompt("Sua idade é maior do que a do seu melhor amigo?", idade)
+
+let numero = undefined
+
+prompt("insira um numero", numero)
+typeof numero (Number)
+// 4%2 0 numeros pares rsto= 0
+//8%4  0
+
+
+
+
+
+
+
+
