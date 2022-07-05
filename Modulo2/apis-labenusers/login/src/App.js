dd43c9de516d1ba1baf7e3fbc90ed1db812e7be1
pedro Vinicius <pedro.carvalho161@gmail.com>
2022-07-04 23:19:09 -0300

    insta lab

---
+++ Modulo2/apis-labenusers/login/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  state={
+    telaAtual:===
+  }
+  render(){
+  return (
+    <div>
+      oi
+    </div>
+  );
+}
+} 
+export default App;
---
+++ Modulo2/use-state/insta-lab-hooks/src/App.js
@@ -0,0 +1,16 @@
+import Post from './components/Post';
+
+function App() {
+  return (
+    <>
+      <h1>Post</h1>
+      <Post
+        nomeUsuario={'Usuário'}
+        fotoUsuario={'https://picsum.photos/50/50'}
+        fotoPost={'https://picsum.photos/200/150'}
+      />
+    </>
+  );
+};
+
+export default App;
